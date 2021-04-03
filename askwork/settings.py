45fbc247c54825f26fe5df651885d8343074ea26
gallapillo <58660325+gallapillo@users.noreply.github.com>
2021-04-03 16:57:08 +0700

    Update settings.py

--- askwork/settings.py
+++ askwork/settings.py
@@ -166,9 +166,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_cdn'),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', "static_root")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', "media_root")
 
 
